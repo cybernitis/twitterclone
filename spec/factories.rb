3cb5c522efacba1b9515fe4ab6de91590abfa1a6
Mario Đurašin <indiche@gmail.com>
2011-10-25 12:42:01 +0200

    Added a User factory and tests for the show page

---
+++ spec/factories.rb
@@ -0,0 +1,6 @@
+Factory.define :user do |user|
+  user.username               "foobar"
+  user.email                  "foo@bar.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
