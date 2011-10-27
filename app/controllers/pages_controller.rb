c8982c4683448cfc5ca4692d5e26afe75494b1ac
Mario Đurašin <indiche@gmail.com>
2011-10-27 15:48:09 +0200

    Added a sign up form

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
 
   def index
     @title = 'Welcome'
+    @user = User.new
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  def new
+
+  def create
   end
 
   def show
