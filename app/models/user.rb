b8c62618fbb90a5b3593caa3d06cefca61d83654
Mario Đurašin <indiche@gmail.com>
2011-10-28 14:48:10 +0200

    Working user sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,8 +21,12 @@ class User < ActiveRecord::Base
   class << self
     def authenticate(username, submitted_password)
       user = find_by_username(username)
-      return nil if user.nil?
-      return user if user.has_password?(submitted_password)
+      (user && user.has_password?(submitted_password)) ? user : nil
+    end
+
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil
     end
   end
 
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -38,5 +38,24 @@ describe SessionsController do
         response.should have_selector('title', :content => "Sign in")
       end
     end
+
+    describe "success" do
+
+      before :each do
+        @user = Factory(:user)
+        @attr = {:username => @user.username, :password => @user.password}
+      end
+
+      it "should sign the user in" do
+        post :create, :session => @attr
+        controller.current_user.should == @user
+        controller.should be_signed_in
+      end
+
+      it "should redirect to users page" do
+        post :create, :session => @attr
+        response.should redirect_to(user_path(@user))
+      end
+    end
   end
 end
